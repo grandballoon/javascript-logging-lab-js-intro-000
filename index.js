10537c6e62fcc94741d8459e2d7ebeaea987bc4c
Luke Conley <luke.sovern.conley@gmail.com>
2017-10-29 20:15:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 console.error("HALP!")
 console.log("Hello")
 console.warn("don't do that")
+var word
+word = 'bird'
+
+console.log(word)
+word = 'dog'
+
+console.log(word)
+var word = 'bird'
+
+var a = 5, b=2, c='hello'
